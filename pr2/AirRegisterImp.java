185511903feacd9fdbed7f223be9f8c955bd2444
srpiroliro <srpiroliro21@gmail.com>
2022-05-24 08:56:35 +0200

    start

---
+++ pr2/AirRegisterImp.java
@@ -0,0 +1,9 @@
+package pr2;
+
+import java.util.*;
+
+public class AirRegisterImp implements AirRegister {
+    private HashMap<Company, HashSet<Aircraft>> companies; // TreeSet?
+    private HashMap<AircraftID, Company> aircrafts;
+    
+}
