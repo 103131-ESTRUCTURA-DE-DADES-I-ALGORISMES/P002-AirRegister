8383ac50909223f1b1a3854018732fa0a48c2a98
srpiroliro <srpiroliro21@gmail.com>
2022-05-30 14:22:56 +0200

    finished.

--- pr2/AirRegisterImp.java
+++ pr2/AirRegisterImp.java
@@ -2,6 +2,7 @@ package pr2;
 
 import java.util.*;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class AirRegisterImp implements AirRegister {    
     private HashMap<Company, TreeSet<Aircraft>> companies=new HashMap<Company, TreeSet<Aircraft>>();
@@ -35,41 +36,26 @@ public class AirRegisterImp implements AirRegister {
     
     @Override
     public Company findCompany(AircraftID id) {
-//    	for (Map.Entry<Company, TreeSet<Aircraft>> contract: companies.entrySet()) {
-//			for(Aircraft a:contract.getValue()) 
-//				if (a.getId().equals(id)) 
-//					return contract.getKey();
-//		}
-//        return null;
-    	SortedSet<Company> result=findC("id", true, id);
-    	return (result.size()>0) ? result.first() : null;
+    	for (Map.Entry<Company, TreeSet<Aircraft>> contract: companies.entrySet()) {
+    		if(contract.getValue().stream().anyMatch(a->a.getId().equals(id)))
+    			return contract.getKey();
+    	}
+    	return null;
+    	
+//    	Solucio massa OP
+//    	return companies.entrySet().stream().filter(e->e.getValue().stream().anyMatch(a->a.getId().equals(id))).map(e->e.getKey()).findFirst().orElse(null);
     }
     
     @Override
     public SortedSet<Company> findCompanyByType(AircraftType t) { 
-//    	SortedSet<Company> tmp=new TreeSet<Company>();
-//    	for (Map.Entry<Company, TreeSet<Aircraft>> contract: companies.entrySet()) {
-//			for(Aircraft a:contract.getValue()) 
-//				if (a.getType().equals(t)) 
-//					tmp.add(contract.getKey());
-//		}
-//        return tmp;
-    	
-    	return findC("type", false, t);
-    }
-    
-    private SortedSet<Company> findC(String type, boolean firstOccurence, Object subject){
     	SortedSet<Company> tmp=new TreeSet<Company>();
-    	for(Map.Entry<Company, TreeSet<Aircraft>> contract: companies.entrySet()) {
-			for(Aircraft a:contract.getValue()) {
-				if((type.equals("id") ? a.getId():a.getType()).equals(subject)) {	
-					tmp.add(contract.getKey());
-					
-					if (firstOccurence)
-						return tmp;
-				}
-			}
+    	for (Map.Entry<Company, TreeSet<Aircraft>> contract: companies.entrySet()) {
+			for(Aircraft a:contract.getValue()) 
+				if (a.getType().equals(t)) tmp.add(contract.getKey());
 		}
         return tmp;
+    	
+//    	Solucio massa OP
+//    	return companies.entrySet().stream().filter(e->e.getValue().stream().anyMatch(a->a.getType().equals(t))).map(c->c.getKey()).collect(Collectors.toCollection(TreeSet::new));
     }
 }
