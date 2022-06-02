bb397306190042031f006fafa4557fe21777bb82
Pol Rubio <49326091+PolRubio@users.noreply.github.com>
2022-06-02 11:18:40 +0200

    no spaces

--- pr2/AirRegisterImp.java
+++ pr2/AirRegisterImp.java
@@ -1,5 +1,4 @@
 package pr2;
-
 import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -9,53 +8,34 @@ public class AirRegisterImp implements AirRegister {
 
     @Override
     public boolean addCompany(Company c) {
-        if(companies.containsKey(c)) 
-        	return false;
-        
+        if(companies.containsKey(c)) return false;
         companies.put(c, new TreeSet<Aircraft>(Comparator.comparing(Aircraft::getYear)));
         return true;
     }
     @Override
     public boolean registerAircraft(Company c, Aircraft a) {
     	Company t=findCompany(a.getId());
-    	if(t!=null && !t.equals(c)) {
-    		throw new DifferentCompanyException("Aquest avió ja està registrat en unaltre companyia");
-    	}
-    	
-        if (companies.containsKey(c)) {
-            return companies.get(c).add(a);
-        }
+    	if(t!=null && !t.equals(c)) throw new DifferentCompanyException("Aquest avió ja està registrat en unaltre companyia");
+        if (companies.containsKey(c)) return companies.get(c).add(a);
         throw new UnknownCompanyException("Aquesta companyia no existeix");
     }
-    
     @Override
     public SortedSet<Aircraft> registeredAircrafts(Company c) {    	
         return (companies.containsKey(c)) ? companies.get(c) : new TreeSet<Aircraft>();
-    }
-    
-    
+    }    
     @Override
     public Company findCompany(AircraftID id) {
     	for (Map.Entry<Company, TreeSet<Aircraft>> contract: companies.entrySet()) {
-    		if(contract.getValue().stream().anyMatch(a->a.getId().equals(id)))
-    			return contract.getKey();
+    		if(contract.getValue().stream().anyMatch(a->a.getId().equals(id))) return contract.getKey();
     	}
-    	return null;
-    	
-//    	Solucio massa OP
-//    	return companies.entrySet().stream().filter(e->e.getValue().stream().anyMatch(a->a.getId().equals(id))).map(e->e.getKey()).findFirst().orElse(null);
+    	return null; //Solucio massa OP: return companies.entrySet().stream().filter(e->e.getValue().stream().anyMatch(a->a.getId().equals(id))).map(e->e.getKey()).findFirst().orElse(null);
     }
-    
     @Override
     public SortedSet<Company> findCompanyByType(AircraftType t) { 
     	SortedSet<Company> tmp=new TreeSet<Company>();
     	for (Map.Entry<Company, TreeSet<Aircraft>> contract: companies.entrySet()) {
-    		if(contract.getValue().stream().anyMatch(a->a.getType().equals(t)))
-				tmp.add(contract.getKey());
+    		if(contract.getValue().stream().anyMatch(a->a.getType().equals(t))) tmp.add(contract.getKey());
 		}
-        return tmp;
-    	
-//    	Solucio massa OP
-//    	return companies.entrySet().stream().filter(e->e.getValue().stream().anyMatch(a->a.getType().equals(t))).map(c->c.getKey()).collect(Collectors.toCollection(TreeSet::new));
+        return tmp; //Solucio massa OP: return companies.entrySet().stream().filter(e->e.getValue().stream().anyMatch(a->a.getType().equals(t))).map(c->c.getKey()).collect(Collectors.toCollection(TreeSet::new));
     }
 }
